12994e2c911602fd0ee8fe34856521cd639993eb
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-08-11 15:31:55 +0100

    Created first feature test

--- app.rb
+++ app.rb
@@ -1,10 +1,13 @@
-require 'sinatra'
-require "sinatra/reloader" if development?
+require 'sinatra/base'
+require "sinatra/reloader"
 
 class Battle < Sinatra::Base
-
+  configure :development do
+    register Sinatra::Reloader
+  end
+  
   get '/' do
-    "Hello Battle!"
+    'Testing infrastructure working!'
   end
 
   # start the server if ruby file executed directly
