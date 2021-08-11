242559a505764ae39a105cc6cfdfd2d6493a24c7
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-08-11 17:20:11 +0100

    Created feature test for player 2 hit points

--- spec/features/app_spec.rb
+++
@@ -1,9 +0,0 @@
-require "./app"
-
-describe Battle do
-  it "should say have homepage message" do
-    visit '/'
-    expect(page).to have_content 'Testing infrastructure working!'
-  end
-end 
-
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -2,6 +2,10 @@ require "./app"
 
 describe Battle do
   it "should display the Hit points of player2" do 
-    expect()
-
+    visit '/'
+    fill_in(:name1, with: 'John')
+    fill_in(:name2, with: 'Joe')
+    click_button('submit')
+    expect(page).to have_content "Joe's HP: 60"
+  end
 end
