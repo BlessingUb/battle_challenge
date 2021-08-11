085e7c71f08f86f262502640e4051f49cb28939c
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-08-11 15:02:35 +0100

    Added message to website, initialized rspec

--- app.rb
+++ app.rb
@@ -2,10 +2,11 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 class Battle < Sinatra::Base
-  # ... app code here ...
-  get '/' do
 
+  get '/' do
+    "Hello Battle!"
   end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
