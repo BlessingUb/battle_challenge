64c1b9f69ca64d871f61cb112ba141af78cb54b6
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-08-11 17:23:39 +0100

    Started using web helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,11 +2,8 @@ require "./app"
 
 describe Battle do
   it "should display submited name on screen" do 
-    visit '/'
-    fill_in(:name1, with: 'John')
-    fill_in(:name2, with: 'Joe')
-    click_button('submit')
-    expect(page).to have_content 'Player 1: John vs Player 2: Joe'
+    sign_in_and_play
+    expect(page).to have_content('Player 1: John vs Player 2: Joe')
   end
 end 
 
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -2,10 +2,7 @@ require "./app"
 
 describe Battle do
   it "should display the Hit points of player2" do 
-    visit '/'
-    fill_in(:name1, with: 'John')
-    fill_in(:name2, with: 'Joe')
-    click_button('submit')
+    sign_in_and_play
     expect(page).to have_content "Joe's HP: 60"
   end
 end
